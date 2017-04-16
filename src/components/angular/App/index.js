91adb286d2eeb63283878f16c0cdef0bd02d0daa
Sergey Bolshchikov <sergey@bolshchikov.net>
2017-04-16 16:13:02 +0300

    improved angular app

--- src/components/angular/App/index.js
+++ src/components/angular/App/index.js
@@ -1,12 +1,30 @@
 import angular from 'angular';
 import s from './App.scss';
 
+class MainAppController {
+  constructor() {
+    this.value = 5;
+  }
+  increase() {
+    this.value += 1;
+  }
+  decrease() {
+    this.value -= 1;
+  }
+}
+
 angular.module('myApp', [])
   .component('mainApp', {
+    controller: MainAppController,
     template: `
       <div class="${s.root}">
         <div class="${s.header}">
           <h2>Hello Angular World!</h2>
+        </div>
+        <div>
+          <span>Counter: "{{$ctrl.value}}"</span>
+          <button ng-click="$ctrl.increase()">+</button>
+          <button ng-click="$ctrl.decrease()">-</button>
         </div>
       </div>
     `
