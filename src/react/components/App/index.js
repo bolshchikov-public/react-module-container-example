5d0dbed82c63a0cf2a66768fa27c11b008129c82
Sergey Bolshchikov <sergey@bolshchikov.net>
2017-04-16 17:18:28 +0300

    refactor to class

--- src/react/components/App/index.js
+++ src/react/components/App/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import s from './App.scss';
 
-const App = () => {
-  return (
-    <div className={s.root}>
-      <div className={s.header}>
-        <h2>{'Hello React World!'}</h2>
+class App extends React.Component {
+  render() {
+    return (
+      <div className={s.root}>
+        <div className={s.header}>
+          <h2>{'Hello React World!'}</h2>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
